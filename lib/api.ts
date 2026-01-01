da4240fb22877e7b906f76a82f3856263b055b80
Syeda-Aliza-Ayaz <syedaalizaayaz.1606@gmail.com>
2026-01-01 14:31:37 +0500

    API_BASE changed

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000/api/accounts";
+const API_BASE = "https://user-management-backend.up.railway.app/api/accounts";
 
 export async function apiFetch(url:string, options: RequestInit = {}) {
     return fetch(`${API_BASE}${url}`, { ...options, headers: { "Content-Type": "application/json", ...(options.headers || {}) }, credentials: "include", });
